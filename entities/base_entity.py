27e29ec804604bdef901f02d428019f6f3c59d8e
Hugo <ht0981.2005@gmail.com>
2025-04-24 19:54:25 -0300

    :sparkles: feat: Adição de lógica à classe da entidade para que ela aja no tick correto

--- entities/base_entity.py
+++ entities/base_entity.py
@@ -56,7 +56,14 @@ class baseEntity:
             True: Caso a entidade possa se  mover
             False: Caso a entidade não possa se mover
         """
-        pass
+        speed = self.get_attribute("speed")
+        start_delay = self.get_attribute("start_delay")
+        map_stats = self.get_attribute("map_stats")
+        current_tick = map_stats.current_tick
+        
+        if current_tick == start_delay or current_tick % speed == 0:
+            return True
+        return False
 
     def act(self):
         """
