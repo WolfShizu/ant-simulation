56cf403484c69909e704334dcb9c02f60f1194f9
Hugo <ht0981.2005@gmail.com>
2025-04-24 19:16:25 -0300

    :sparkles: feat: Adição do gerenciador do mapa e classe para salvar o status do mapa

---
+++ map/map_handler.py
@@ -0,0 +1,6 @@
+from map_stats import MapStats
+
+map_stats = MapStats()
+
+def add_entity(entity):
+    entity.map_stats = map_stats
---
+++ map/map_stats.py
@@ -0,0 +1,4 @@
+class MapStats:
+    def __init__(self):
+        self.current_tick = 1
+        self.daytime = True
