2d4e961a3d03b5466cccf957ba026c3d7c37dec5
Hugo <ht0981.2005@gmail.com>
2025-04-25 22:36:58 -0300

    :bulb: docs: Adição de comentários aos módulos de mapa

--- map/map_handler.py
+++ map/map_handler.py
@@ -1,6 +1,10 @@
 from map.map_stats import MapStats
-
-map_stats = MapStats()
+map_stats = MapStats() # Cria o objeto que guarda os atributos do mapa
 
 def get_map_stats():
+    """
+    Retorna a referência ao objeto dos atributos do mapa
+    Returns:
+        MapStats: Instância atual de MapStats
+    """
     return map_stats
--- map/map_stats.py
+++ map/map_stats.py
@@ -1,4 +1,10 @@
 class MapStats:
+    """
+    Classe responsável pelos atributos do mapa
+    """
     def __init__(self):
+        """
+        Inicia a instância com as configurações básicas do mapa
+        """
         self.current_tick = 1
         self.daytime = True
